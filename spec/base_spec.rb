8fed6b3646b59e4465fa62a0b4f6da4d7b7f759c
Tony Arcieri <tony@medioh.com>
2010-08-20 20:53:30 -0600

    Wrap all job declarations in a workflow block

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -4,25 +4,27 @@ class CatWashWorkflow < Foreman::Base
   valid_states  :new, :cat_caught, :in_bathroom
   default_state :new
   
-  # Catch the cat
-  job_for :new, :success => :cat_caught do
-    puts "Catching the cat..."
-    # come_here_kitty.no_dont_run_away.gaaaah.got_you.owwwww_stop_it
-    puts "Cat caught!"
-  end
-  
-  # Walk to bathroom
-  job_for :cat_caught, :success => :in_bathroom, :fail => :new do
-    puts "Walking to the bathroom..."
-    # dont_jump_out_of_my_hands_you_little_bugger
-    unless @never_works_the_first_time
-      @never_works_the_first_time = true
-      
-      msg = "Uhoh, she sensed I was gonna bathe her and got away"
-      puts msg
-      raise msg
+  workflow do
+    # Catch the cat
+    job_for :new, :success => :cat_caught do
+      puts "Catching the cat..."
+      # come_here_kitty.no_dont_run_away.gaaaah.got_you.owwwww_stop_it
+      puts "Cat caught!"
     end
-    puts "Yay at least got to the bathroom"
+  
+    # Walk to bathroom
+    job_for :cat_caught, :success => :in_bathroom, :fail => :new do
+      puts "Walking to the bathroom..."
+      # dont_jump_out_of_my_hands_you_little_bugger
+      unless @never_works_the_first_time
+        @never_works_the_first_time = true
+      
+        msg = "Uhoh, she sensed I was gonna bathe her and got away"
+        puts msg
+        raise msg
+      end
+      puts "Yay at least got to the bathroom"
+    end
   end
 end
 

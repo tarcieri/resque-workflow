97ea10d65c4ce0071ca6d684bb5d71c81973fa8d
Tony Arcieri <tony@medioh.com>
2010-10-08 11:59:54 -0600

    Update methoddefinitions to support new DSL

--- lib/foreman.rb
+++ lib/foreman.rb
@@ -6,14 +6,22 @@ module Foreman
 
   module ClassMethods
     # Set the default state of this workflow
-    def default_state(state)
-      @__default_state = state.to_sym
-    end
-  
     # Explicitly specify the states of the workflow
-    def valid_states(*states)
-      @__valid_states = states.map { |state| state.to_sym }
-      validates_inclusion_of :state, :in => @__valid_states
+    def states(*states)
+      if states.last.is_a? Hash
+        options = states.pop
+      else
+        options = {}
+      end
+      
+      @__valid_states  = states.map { |state| state.to_sym }
+      @__default_state = options[:default].to_sym || @__valid_states.first
+      
+      unless valid_states.include? default_state
+        raise ArgumentError, "default state '#{@__default_state}' is invalid"
+      end
+      
+      validates_inclusion_of :state, :in => valid_states
     end
   
     # Declare a workflow
@@ -24,6 +32,12 @@ module Foreman
     # Specify a job for a given state
     def job_for(state, options = {}) 
     end
+    
+    # Return the list of valid sates    
+    def valid_states; @__valid_states; end
+    
+    # Return the default state
+    def default_state; @__default_state; end
   end
   
   def self.included(klass)

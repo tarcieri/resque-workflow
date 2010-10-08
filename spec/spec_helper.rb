9ccf6017cf900db1309c9f36654cdd2c237f635c
Tony Arcieri <tony@medioh.com>
2010-10-08 12:37:48 -0600

    Create a test sqlite database and populate its schema

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,18 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rubygems'
 require 'foreman'
 require 'rspec'
+
+database = "test.sqlite3"
+File.delete database if File.exist? database
+ActiveRecord::Base.establish_connection :adapter  => 'sqlite3',
+                                        :database => database
+                                        
+ActiveRecord::Migration.verbose = false
+
+ActiveRecord::Schema.define(:version => 1) do
+  create_table :cats, :force => true do |t|
+    t.string :workflow_state
+    t.string :name
+    t.string :mood, :default => 'fiesty'
+  end
+end
